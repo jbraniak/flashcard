411f5e24383f9801620bef05e1dc947ce2caf4b5
jbraniak <143524432+jbraniak@users.noreply.github.com>
2024-11-01 08:05:00 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,31 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
+    'Comida',
+    'Quem é conhecido como o "Rei dos Queijos"??',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Em qual país se originou o strogonoff?',
+    'A capital da França é Otawwa.'
 )
 
 criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
